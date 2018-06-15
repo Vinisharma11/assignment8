a8d71e563c9b7bd58d8c8e8325f0f32aa4d60ac4
Vinisharma11 <sharvi786@gmail.com>
2018-06-15 19:35:57 +0530

    initial commit

--- pattern.java
+++ pattern.java
@@ -2,15 +2,25 @@ class Pattern
 {
   public static void main(String abc[])
   {
+	  int k=6;
+	  
    for(int i=1;i<4;i++)
    {
-   int alpha=97;                          
-   for(int j=1;j<=i;j++)
-   {
-   System.out.print((char)alpha);
-   alpha++;
+	   int alpha=97;
+	    
+      for(int j=0; j<k;j++) 
+	  {		  
+         System.out.print(" ");  
+	  }		 
+	  k=k-1;
+      for(int j=1;j<=i;j++)
+       {
+         System.out.print(" "+(char)alpha);
+         alpha++;
+       }
+	 
+      System.out.println(" ");
    }
-   System.out.println();
-   }
+ 
   }
 }
